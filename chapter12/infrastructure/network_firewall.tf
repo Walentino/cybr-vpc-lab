61ecec668b3057efce49a81e4b266320675b1c25
Brandon Carroll <bcrrll@Brandons-MacBook-Pro-2.local>
2025-02-05 10:33:26 -0800

    several chapter comamnd lists and resources

---
+++ chapter12/infrastructure/network_firewall.tf
@@ -0,0 +1,17 @@
+resource "aws_networkfirewall_firewall" "main" {
+  name                = "network-firewall-${var.environment}"
+  firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
+  vpc_id             = data.aws_vpc.main.id
+  
+  subnet_mapping {
+    subnet_id = data.aws_subnet.firewall.id
+  }
+}
+
+data "aws_vpc" "main" {
+  id = "vpc-0488a892d43711307"  # Your existing VPC ID
+}
+
+data "aws_subnet" "firewall" {
+  id = "subnet-064cd6599825d706d"  # Your existing subnet ID
+}
