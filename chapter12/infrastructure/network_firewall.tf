f5b745758639d4a1ce0d4884f3f7b8efc53d6bc7
Brandon Carroll <brandon.carroll@globalconfig.net>
2025-02-06 13:08:41 -0800

    Update network_firewall.tf

--- chapter12/infrastructure/network_firewall.tf
+++ chapter12/infrastructure/network_firewall.tf
@@ -1,6 +1,6 @@
 resource "aws_networkfirewall_firewall" "main" {
   name                = "network-firewall-${var.environment}"
-  firewall_policy_arn = arn:aws:network-firewall:us-west-2:387974667323:firewall-policy/AllowAllTrafficPolicy
+  firewall_policy_arn = "arn:aws:network-firewall:us-west-2:387974667323:firewall-policy/AllowAllTrafficPolicy"
   vpc_id              = data.aws_vpc.main.id
 
   subnet_mapping {
