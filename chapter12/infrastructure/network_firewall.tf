b1539031725463814fc17bc3dbc538b4ecfd6b85
Brandon Carroll <brandon.carroll@globalconfig.net>
2025-02-06 11:34:33 -0800

    Update network_firewall.tf
    FIX FORMAT

--- chapter12/infrastructure/network_firewall.tf
+++ chapter12/infrastructure/network_firewall.tf
@@ -1,17 +1,17 @@
 resource "aws_networkfirewall_firewall" "main" {
   name                = "network-firewall-${var.environment}"
   firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
-  vpc_id             = data.aws_vpc.main.id
-  
+  vpc_id              = data.aws_vpc.main.id
+
   subnet_mapping {
     subnet_id = data.aws_subnet.firewall.id
   }
 }
 
 data "aws_vpc" "main" {
-  id = "vpc-0488a892d43711307"  # Your existing VPC ID
+  id = "vpc-0488a892d43711307" # Your existing VPC ID
 }
 
 data "aws_subnet" "firewall" {
-  id = "subnet-064cd6599825d706d"  # Your existing subnet ID
+  id = "subnet-064cd6599825d706d" # Your existing subnet ID
 }
