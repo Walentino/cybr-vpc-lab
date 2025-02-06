3a5765b9d6bcc6851c3c9b8c66faa435eb245b3a
Brandon Carroll <bcrrll@Brandons-MacBook-Pro-2.local>
2025-02-06 09:23:17 -0800

    trying something again

--- chapter12/infrastructure/backend.tf
+++ chapter12/infrastructure/backend.tf
@@ -7,7 +7,7 @@ terraform {
     region         = "us-west-2" # locals.s3_bucket_name
     encrypt        = true
     dynamodb_table = "terraform_state_lock"
-    bucket = "arn:aws:s3:::terraformstatebucketsecuringtheawscloud"
+    bucket = "terraformstatebucketsecuringtheawscloud"
     key = "123"
   }
 }
